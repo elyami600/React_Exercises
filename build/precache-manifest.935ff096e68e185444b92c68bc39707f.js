50dd8cf82cb8818301d8411f596ed550ea00ae51
Yamil Perez <42903867+elyami600@users.noreply.github.com>
2021-09-15 18:05:34 -0400

    React files

---
+++ build/precache-manifest.935ff096e68e185444b92c68bc39707f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dbbce163d14206f2a52d22517a097ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "490bdd84ae007acbfb02",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "98089c2ca4c07b83edce",
+    "url": "/static/js/2.d27cf73e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d27cf73e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "490bdd84ae007acbfb02",
+    "url": "/static/js/main.f42ba10f.chunk.js"
+  },
+  {
+    "revision": "5e4360652bb859cb3c83",
+    "url": "/static/js/runtime-main.d33e3326.js"
+  }
+]);
